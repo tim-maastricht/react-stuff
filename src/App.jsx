fe5bf058f1b74dacb5a5367bfbbb580bca80379c
Tim <tim.maastricht@gmail.com>
2026-05-06 20:08:38 +1000

    feat: add pokemon name and sprite renderer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import RandomPokemon from './components/RandomPokemon'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <section id="center">
+        <RandomPokemon />
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
--- src/components/RandomPokemon.jsx
+++ src/components/RandomPokemon.jsx
@@ -1,4 +1,38 @@
+import { useState, useEffect } from 'react'
+
 export default function RandomPokemon() {
+  const [pokemonName, setPokemonName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    console.log("hello from randopokemon")
+    
+    // async/await must happen inside hook
+    async function getRandomPokemon(){
+      // generate random number
+      let randomPokemonId = Math.floor(Math.random() * 1025 + 1)
+      //make fetch req using random number
+      let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomPokemonId);
+      // save result
+      let responseBody = await response.json();
+      // pass the result into setters
+      setPokemonName(responseBody.name);
+      setImageUrl(responseBody.sprites.front_default);
+    }
+    getRandomPokemon();
+    // return in a useEffect = componentWillUnmount
+    return (() => {
+      console.log("Component will unmount")
+    })
+
+    // empty dependency array = componentDidMount
+  }, []);
+
+  // useEffect(() => { // this is an example of a dependency array changing the pokemon
+  //   console.log("pokemon name changed!" + pokemonName)
+  //   // putting var into depArr turns useEffect into componentDidUpdate
+  // }, [pokemonName])
+
   return (
     <div>
       <h1>{pokemonName}</h1>
